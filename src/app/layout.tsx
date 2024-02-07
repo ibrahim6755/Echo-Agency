fef109866319df2760ce2d0d1b0652aef9ceb729
ibrahim6755 <ibrahimghani020@gmail.com>
2024-02-07 08:04:52 +0500

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,28 +2,28 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
- 
-export const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-})
- 
+
+const inter = Inter({ subsets: ["latin"] });
+
 export const metadata: Metadata = {
-  title: "Eco Agency",
-  description: "We Deliver the Best Services",
+  title: "Echo Agency",
+  description: "We  are Echo, a digital agency specializing in creating innovative websites and applications.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
