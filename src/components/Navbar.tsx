e701f20af281a8313e1e13b471d5eddefada0656
ibrahim6755 <ibrahimghani020@gmail.com>
2024-02-08 18:15:20 +0500

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,6 +15,7 @@ import {
 import { ChevronDown, Menu } from "lucide-react";
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
@@ -92,77 +93,117 @@ function Navbar() {
             />
           </Link>
           <div className="div flex ">
-          <li className="list-none mx-2 text-sm cursor-pointer">
+            <li className="list-none mx-2 text-sm cursor-pointer">
               <Toggle />
             </li>
-          <Sheet>
-            <SheetTrigger>
-              <Menu />
-            </SheetTrigger>
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>
-                  {" "}
-                  <Image
-                    src="/images/echo-logo.jpg"
-                    alt="company-logo"
-                    height={80}
-                    width={80}
-                  />
-                </SheetTitle>
-                <SheetDescription>
-                  <div className="links mt-10">
-                    <ul className="flex flex-col items-start">
-                      <li className="list-none my-4  text-sm cursor-pointer custom_grey">
-                        Home
-                      </li>
-                      <li className="list-none my-4  text-sm cursor-pointer custom_grey">
-                        <DropdownMenu>
-                          <DropdownMenuTrigger className="outline-none flex items-center justify-center ">
-                            Dropdown
-                            <ChevronDown className="text-sm custom_grey py-0 my-0" />
-                          </DropdownMenuTrigger>
-                          <DropdownMenuContent className="mx-48">
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem className="custom_grey">
-                              Profile
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className="custom_grey">
-                              Billing
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className="custom_grey">
-                              Team
-                            </DropdownMenuItem>
-                            <DropdownMenuItem className="custom_grey">
-                              Subscription
-                            </DropdownMenuItem>
-                          </DropdownMenuContent>
-                        </DropdownMenu>
-                      </li>
-                      <li className="list-none my-4  text-sm cursor-pointer custom_grey">
-                        About
-                      </li>
-                      <li className="list-none my-4  text-sm cursor-pointer custom_grey">
-                        Services
-                      </li>
-                      <li className="list-none my-4  text-sm cursor-pointer custom_grey">
-                        FAQ
-                      </li>
+            <Sheet>
+              <SheetTrigger>
+                <Menu />
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>
+                    {" "}
+                    <Image
+                      src="/images/echo-logo.jpg"
+                      alt="company-logo"
+                      height={80}
+                      width={80}
+                    />
+                  </SheetTitle>
+                  <SheetDescription>
+                    <div className="links mt-10">
+                      <ul className="flex flex-col items-start">
+                        <SheetClose asChild>
+                          <Button type="submit">
+                            <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                              Home
+                            </li>
+                          </Button>
+                        </SheetClose>
+                        <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                          <DropdownMenu>
+                            <DropdownMenuTrigger className="ms-4 outline-none flex items-center justify-center ">
+                              Dropdown
+                              <ChevronDown className="text-sm custom_grey py-0 my-0" />
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="mx-48">
+                              <DropdownMenuSeparator />
+                              <DropdownMenuItem className="custom_grey">
+                                <SheetClose asChild>
+                                  <Button type="submit">
+                                    <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                                      Profile
+                                    </li>
+                                  </Button>
+                                </SheetClose>
+                              </DropdownMenuItem>
+                              <DropdownMenuItem className="custom_grey">
+                                <SheetClose asChild>
+                                  <Button type="submit">
+                                    <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                                      Billing
+                                    </li>
+                                  </Button>
+                                </SheetClose>
+                              </DropdownMenuItem>
+                              <DropdownMenuItem className="custom_grey">
+                                <SheetClose asChild>
+                                  <Button type="submit">
+                                    <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                                      Team
+                                    </li>
+                                  </Button>
+                                </SheetClose>
+                              </DropdownMenuItem>
+                              <SheetClose asChild>
+                                <Button type="submit">
+                                  <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                                    Subscription
+                                  </li>
+                                </Button>
+                              </SheetClose>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </li>
+                        <SheetClose asChild>
+                          <Button type="submit">
+                            <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                              About
+                            </li>
+                          </Button>
+                        </SheetClose>
+                        <SheetClose asChild>
+                          <Button type="submit">
+                            <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                              Services
+                            </li>
+                          </Button>
+                        </SheetClose>
+                        <SheetClose asChild>
+                          <Button type="submit">
+                            <li className="list-none my-4  text-sm cursor-pointer custom_grey">
+                              FAQ
+                            </li>
+                          </Button>
+                        </SheetClose>
 
-                      <li className="list-none my-4 text-sm cursor-pointer custom_grey">
-                        <Button
-                          variant="ghost"
-                          className="custom_bg_blue custom_yellow rounded-full border border-transparent hover:border-gray-200 transition duration-300"
-                        >
-                          Contact Us
-                        </Button>
-                      </li>
-                    </ul>
-                  </div>
-                </SheetDescription>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
+                        <li className="list-none my-4 text-sm cursor-pointer custom_grey">
+                          <Button
+                            variant="ghost"
+                            className="custom_bg_blue custom_yellow rounded-full border border-transparent hover:border-gray-200 transition duration-300"
+                          >
+                            <SheetClose asChild>
+                              <Button >Contact Us</Button>
+                            </SheetClose>
+                          </Button>
+                        </li>
+                      </ul>
+                    </div>
+                  </SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
           </div>
         </nav>
       </header>
